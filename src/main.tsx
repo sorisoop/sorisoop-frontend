61c07b02df8265b76d5883480a4550bb714f472d
sangzun-han <hsjun1996@naver.com>
2025-08-25 21:59:51 +0900

    fix: 노치 수정

--- src/main.tsx
+++ src/main.tsx
@@ -37,7 +37,7 @@ const isStandalone =
   window.matchMedia("(display-mode: standalone)").matches ||
   ("standalone" in window.navigator && (window.navigator as { standalone?: boolean }).standalone === true);
 
-if (isStandalone) document.documentElement.style.setProperty("--safe-area-top", "0px");
+if (isStandalone) document.documentElement.style.setProperty("--safe-area-top", "max(20px, env(safe-area-inset-top))");
 else document.documentElement.style.setProperty("--safe-area-top", "env(safe-area-inset-top)");
 
 createRoot(document.getElementById("root")!).render(
