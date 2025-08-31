d6a5f3773280a26b1281e8c056de3b4b72d244b3
sangzun-han <hsjun1996@naver.com>
2025-08-31 17:53:59 +0900

    fix: tts 받기 전 warning 제거

--- src/pages/fairy-tale/detail/read/index.tsx
+++ src/pages/fairy-tale/detail/read/index.tsx
@@ -4,9 +4,13 @@ import { FairyTaleReaderProvider } from "@/features/fairy-tale/providers";
 import { FairyTaleReader } from "@/features/fairy-tale/components/reader";
 
 export default function FairyTaleReaderPage() {
-  const { id, voiceUuid } = useParams<{ id: string; voiceUuid: string }>();
+  const { id } = useParams<{ id: string; voiceUuid: string }>();
   const fairyTaleId = id ? Number(id) : undefined;
-  const voiceId = voiceUuid ? Number(voiceUuid) : undefined;
+  //   const voiceId = voiceUuid ? Number(voiceUuid) : undefined;
+
+  if (!fairyTaleId) {
+    return <div className="p-4">잘못된 접근입니다.</div>;
+  }
 
   return (
     <FairyTaleReaderProvider id={fairyTaleId}>
