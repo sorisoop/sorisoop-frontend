7ea23b93190b63ddbe942132ee4a6cb98556ab4d
sangzun-han <hsjun1996@naver.com>
2025-09-03 15:47:07 +0900

    feat : polyfill

--- src/features/notification/notification-sse-listener.tsx
+++ src/features/notification/notification-sse-listener.tsx
@@ -3,6 +3,7 @@ import { useLocation } from "react-router-dom";
 import { toast } from "sonner";
 import { useIsWebview } from "@/shared/hooks/use-is-webview";
 import { WebViewFacade } from "@/shared/webview";
+import { EventSourcePolyfill } from "event-source-polyfill";
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 export default function NotificationSseListener() {
@@ -15,12 +16,13 @@ export default function NotificationSseListener() {
   useEffect(() => {
     if (!shouldConnect) return;
 
-    const es = new EventSource(`${API_BASE_URL}/notifications/subscribe`, {
+    const es = new EventSourcePolyfill(`${API_BASE_URL}/notifications/subscribe`, {
       withCredentials: true,
     });
 
     es.addEventListener("make-fairy-tale-complete", (e) => {
       const data = (e as MessageEvent).data;
+      console.log(data);
       if (isWebView) WebViewFacade.sendNotification(data);
       else toast.success(`동화 생성이 완료됐습니다.`);
     });
