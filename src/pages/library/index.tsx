5b1017e9db89d183fd090e156da7ccc5747a15a5
sangzun-han <hsjun1996@naver.com>
2025-09-08 23:09:46 +0900

    feat: lib errorboundary

--- src/pages/library/index.tsx
+++ src/pages/library/index.tsx
@@ -1,23 +1,26 @@
-import { CommonLayout } from "@/shared/layouts";
 import { Suspense, useState } from "react";
+import { ErrorBoundary } from "react-error-boundary";
+import { CommonLayout } from "@/shared/layouts";
 import { FairyTaleCard } from "@/features/fairy-tale/components/variants";
 import { Tabs, TabsList, TabsTrigger, TabsContent } from "@/shared/components/ui/tabs";
 import FavoriteFairyTaleContent from "./favorite-fairy-tale-content";
 import CustomFairyTaleContent from "./custom-fairy-tale-content";
 import { CategoriesNav, CategoriesNavSkeleton } from "@/widgets";
+import { ErrorFallback } from "@/shared/components/error-boundary";
 
 export default function LibraryPage() {
   const [categoryId, setCategoryId] = useState(0);
 
   return (
     <CommonLayout title="내 책장">
-      <Tabs defaultValue="favorite" className="w-full">
-        <div className="relative left-1/2 right-1/2 -mx-[50vw] w-screen">
-          <div className="mx-auto max-w-screen-xl">
-            <TabsList className="grid grid-cols-2 w-full p-0 bg-transparent h-auto rounded-none shadow-none !border-0 !border-transparent !divide-x-0">
-              <TabsTrigger
-                value="favorite"
-                className="relative w-full flex justify-center py-3 text-base font-semibold
+      <ErrorBoundary fallback={<ErrorFallback />}>
+        <Tabs defaultValue="favorite" className="w-full">
+          <div className="relative left-1/2 right-1/2 -mx-[50vw] w-screen">
+            <div className="mx-auto max-w-screen-xl">
+              <TabsList className="grid grid-cols-2 w-full p-0 bg-transparent h-auto rounded-none shadow-none !border-0 !border-transparent !divide-x-0">
+                <TabsTrigger
+                  value="favorite"
+                  className="relative w-full flex justify-center py-3 text-base font-semibold
                 rounded-none border-0 bg-transparent shadow-none
                 hover:text-foreground hover:bg-transparent
                 data-[state=active]:text-primary
@@ -25,12 +28,12 @@ export default function LibraryPage() {
                 before:scale-x-0 before:transition-transform before:duration-300
                 data-[state=active]:before:scale-x-100
                 min-h-[calc(1.5rem+1.5rem+2px)]"
-              >
-                찜한 동화
-              </TabsTrigger>
-              <TabsTrigger
-                value="my"
-                className="relative w-full flex justify-center py-3 text-base font-semibold
+                >
+                  찜한 동화
+                </TabsTrigger>
+                <TabsTrigger
+                  value="my"
+                  className="relative w-full flex justify-center py-3 text-base font-semibold
                     rounded-none border-0 bg-transparent shadow-none
                     hover:text-foreground hover:bg-transparent
                     data-[state=active]:text-primary
@@ -38,29 +41,30 @@ export default function LibraryPage() {
                     before:scale-x-0 before:transition-transform before:duration-300
                     data-[state=active]:before:scale-x-100
                     min-h-[calc(1.5rem+1.5rem+2px)]"
-              >
-                내가 만든 동화
-              </TabsTrigger>
-            </TabsList>
+                >
+                  내가 만든 동화
+                </TabsTrigger>
+              </TabsList>
+            </div>
           </div>
-        </div>
 
-        <TabsContent value="favorite">
-          <Suspense fallback={<FairyTaleCard.GridSkeleton />}>
-            <FavoriteFairyTaleContent />
-          </Suspense>
-        </TabsContent>
+          <TabsContent value="favorite">
+            <Suspense fallback={<FairyTaleCard.GridSkeleton />}>
+              <FavoriteFairyTaleContent />
+            </Suspense>
+          </TabsContent>
 
-        <TabsContent value="my">
-          <Suspense fallback={<CategoriesNavSkeleton />}>
-            <CategoriesNav categoryId={categoryId} onChange={setCategoryId} />
-          </Suspense>
+          <TabsContent value="my">
+            <Suspense fallback={<CategoriesNavSkeleton />}>
+              <CategoriesNav categoryId={categoryId} onChange={setCategoryId} />
+            </Suspense>
 
-          <Suspense fallback={<FairyTaleCard.GridSkeleton />}>
-            <CustomFairyTaleContent categoryId={categoryId} />
-          </Suspense>
-        </TabsContent>
-      </Tabs>
+            <Suspense fallback={<FairyTaleCard.GridSkeleton />}>
+              <CustomFairyTaleContent categoryId={categoryId} />
+            </Suspense>
+          </TabsContent>
+        </Tabs>
+      </ErrorBoundary>
     </CommonLayout>
   );
 }
