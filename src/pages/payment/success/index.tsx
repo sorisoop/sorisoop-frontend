b15ed99e0728b4ed4309de96400668ad70a786f1
sangzun-han <hsjun1996@naver.com>
2025-09-11 18:42:12 +0900

    fix: 결제 성공 시 마이페이지로

--- src/pages/payment/success/index.tsx
+++ src/pages/payment/success/index.tsx
@@ -18,7 +18,7 @@ export default function PaymentSuccessPage() {
 
       if (orderId && paymentKey && amount && planType) {
         await startSubscription({ orderId, paymentKey, amount, planType });
-        navigate("/subscription/manage", { replace: true });
+        navigate("/my", { replace: true });
       }
     }
 
